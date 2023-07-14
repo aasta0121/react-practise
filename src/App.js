0137a41a13869df40ec598e6c094ade8b272c47c
2100536 <aastalin@systex.com>
2023-07-14 11:29:05 +0800

    day1today10

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App0() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button>大家好</button>
   );
 }
 
+/* 第一個練習 Start */
+const styleArgument = { backgroundColor: 'black', color: 'white' };
+const getValue = (value) => {
+  console.log(value)
+}
+
+const multiButton = () => {
+  var output = [];
+  for (let i = 0; i < 4; ++i) {
+    output.push(
+      <button
+        value={i}
+        style={styleArgument}
+        onClick={(event) => { getValue(event.target.value) }}>
+        我是第{i}個按鍵
+      </button>
+    )
+  }
+  return output;
+}
+
+function App1() {
+  return (<div>{multiButton()}</div>)
+}
+
+/* 第一個練習 End */
+
+/* 第二個練習 Start */
+function App2() {
+  const barWidth = "50%";
+  return (
+    <div>
+      <div className="progress-back" style={{ backgroundColor: "rgba(0,0,0,0.2)", width: "200px", height: "7px", borderRadius: "10px" }}>
+        <div className="progress-bar" style={{ backgroundColor: "#fe5196", width: barWidth, height: "100%", borderRadius: "10px" }}></div>
+      </div>
+    </div>
+  );
+}
+/* 第二個練習 End */
+
+
+/* 第三個練習 Start */
+//props是唯讀變數
+function App3(props) {
+  return (
+    <button>{props.name},我{props.age}歲</button>
+  );
+}
+/* 第三個練習 End */
+
+/* 第四個練習 Start */
+//props是唯讀變數
+function App4(props) {
+  return (
+    <button onClick={props.handleClick}>{props.name}</button>
+  );
+}
+/* 第四個練習 End */
+
+/* 第五個練習 Start */
+//children常用來做Layout
+function App(props) {
+  return (
+    <button>{props.children}</button>
+  );
+}
+/* 第五個練習 End */
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,24 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const printMessage = () => {
+  document.getElementById('show-area').innerHTML = "我被按到了";
+}
+
+/*
 root.render(
   <React.StrictMode>
-    <App />
+    <App name="Aasta" handleClick={printMessage} />
+    <div id="show-area"></div>
   </React.StrictMode>
 );
+*/
 
+root.render(
+  <React.StrictMode>
+    <App>在index.js中設定文字</App>
+  </React.StrictMode>
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
